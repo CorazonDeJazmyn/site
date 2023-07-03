4a67951d3f847f96b9b41477e23dece05b503284
corazondejazmyn <corazondejazmyn@gmail.com>
2023-07-03 08:42:05 +0200

    Fixed images path and contact styles

--- src/app/shared/services/db.service.ts
+++ src/app/shared/services/db.service.ts
@@ -16,27 +16,27 @@ export class DBService {
     const listEarrings: GalleryItem[] = [];
 
     listEarrings.push(new GalleryItem({
-      image: '../../../assets/images/img_pendiente_colibri.jpg',
+      image: './assets/images/img_pendiente_colibri.jpg',
       title: 'Colibri',
       description: 'El colibrí es ideal para rostros pequeños y finos. Con un diseño original pero sencillo, puedes elegir los colores del cuerpo y las alas.'
     }));
     listEarrings.push(new GalleryItem({
-      image: '../../../assets/images/img_pendiente_lobo.jpg',
+      image: './assets/images/img_pendiente_lobo.jpg',
       title: 'Lobo',
       description: 'El lobo feliz. Invoca al espíritu del lobo que llevas dentro, es dulce, pero también tiene garra y desgarra... cuando es necesario.'
     }));
     listEarrings.push(new GalleryItem({
-      image: '../../../assets/images/img_pendiente_luna.jpg',
+      image: './assets/images/img_pendiente_luna.jpg',
       title: 'Luna',
       description: 'La luna en sus diferentes fases. Prueba con diferentes colores para crear tu propio diseño.'
     }));
     listEarrings.push(new GalleryItem({
-      image: '../../../assets/images/img_pendiente_mandala.jpg',
+      image: './assets/images/img_pendiente_mandala.jpg',
       title: 'Mandala',
       description: 'Lo puedes pedir con o sin tiras, y en los colores que más te gusten.'
     }));
     listEarrings.push(new GalleryItem({
-      image: '../../../assets/images/img_pendiente_rainbow.jpg',
+      image: './assets/images/img_pendiente_rainbow.jpg',
       title: 'Rainbow',
       description: 'Ideales para acudir a un encuentro LGTBI+ o a un rainbow.'
     }));
