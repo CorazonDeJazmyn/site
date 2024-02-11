7304361e34fd6dc31be2e29220d1e7d5bdedbf89
corazondejazmyn <corazondejazmyn@gmail.com>
2024-02-11 23:24:55 +0100

    Auto-generated commit [ci skip]

--- assets/images/images.ts
+++ assets/images/images.ts
@@ -5,7 +5,18 @@ export class IMAGES {
 
   static imgWelcomeBack = './assets/images/earrings/img_pendiente_luna_1.jpg';
 
+  static QR_Instagram = './assets/images/qr_instagram.png';
+  static brand_instagram = './assets/images/instagram.svg';
+
   // SVG
+  static svgInstagram = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M16.5 7.5l0 .01" />
+    </svg>
+  `;
+
   static svgStudy = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.813 23.453">
       <path id="Path_15" data-name="Path 15" d="M4099.584,972.736h0l-10.882,3.9,1.637,14.4,9.245,5.153,9.245-5.153,1.686-14.4Z" transform="translate(-4088.702 -972.736)" fill="#ffa726"/>
